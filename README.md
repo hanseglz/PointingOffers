ef23be07c7d610d6d3e30776d23d85c73bfe20aa
hanseglz <hanse971126@gmail.com>
2021-08-24 21:38:27 -0400

    add stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+### NOT A README
+
+**This is a personal project for recording my 刷题 procedure.**
+
+https://leetcode.com/problemset/all
---
+++ src/xhs/daily210802.py
@@ -0,0 +1,35 @@
+#!usr/bin/python3
+
+'''
+    Given a binary tree, and a target number,
+    find if there is a path from the root to any leaf
+    that sums up to the target.
+
+    ***This question is not finished yet due to lack of testing examples.***
+'''
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def target_sum_bst(root, target):
+    # Fill this in
+    if root is None:
+        pass
+
+    res = [root.value]
+    sum += root.value
+    while (root.left):
+        sum += 1
+    pass
+
+n6 = Node(6)
+n4 = Node(4)
+n3 = Node(3, None, n4)
+n2 = Node(2, None, n6)
+n1 = Node(1, n2, n3)
+print(target_sum_bst(n1, 9))
+# True
+# Path from 1 -> 2 -> 6
